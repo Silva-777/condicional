5ec68eb107b415e34c68d9048498f40d612b5f8b
Silva-777 <rocha.andre7777@gmail.com>
2024-10-16 15:43:47 -0300

    first commit

---
+++ scripit.js
@@ -0,0 +1,30 @@
+// condicional é uma forma de forma decisões, de fazer o computador executar essas decisões 
+
+// if signifca "se"
+// else significa "se não"
+
+// if (condição) {códiogo que será executado}
+
+// variaveis
+// 3 var, let e const
+// propt pergunta algo ao usuario;
+// let nome = prompt("Qual é o seu nome?")
+
+// alert usamos para exibir uma mensagem
+// alert("Olá Seja bem vindo(a), " + nome)
+
+// let idade = prompt("Quantos anos você tem?");
+
+// if (idade >= 18){
+//     alert("Você ja pode tirar a carteira de motorista!!!!")
+// } else (idade < 18) {
+//     alert("Você ainda não tem idade para dirigir")
+// }
+
+let gostarDePizza = prompt(" Você gosta de pizza? respoda (sim ou não)");
+// === estreitamente
+if (gostarDePizza === "sim") {
+    alert("Qur ótimo! Vamos comer juntos !")
+} else{
+    alert("Tudo bem, talvez você goste de outra comida")
+}
